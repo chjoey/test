5eb0613ca54de8775b53a47d8f5a97568ebe7276
joey <173899378@qq.com>
2018-04-02 16:56:14 +0800

    init

---
+++ source/joey/thread/cyclicBarrier/TestCyclicBarry.java
@@ -0,0 +1,42 @@
+package joey.thread.cyclicBarrier;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * Created by yp-tc-m-7179 on 2018/3/21.
+ */
+public class TestCyclicBarry extends Thread {
+
+    private int interval;
+
+    TestCyclicBarry(int second) {
+        interval = second;
+    }
+
+    static CyclicBarrier c = new CyclicBarrier(5);
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(interval * 1000);
+            System.out.println(interval + "一阶段完成");
+            c.await();
+            Thread.sleep(interval * 1000);
+            System.out.println(interval + " 二阶段完成");
+            c.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+//        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
+            new TestCyclicBarry(i).start();
+        }
+//        c.await();//i<5时可以尝试在主线程处理
+        System.out.println("~~");
+    }
+
+}
